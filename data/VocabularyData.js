bbff1eaf537ec8791f6647c721cfdf48cf21d0b8
15520065 <15520065@gm.uit.edu.vn>
2018-11-01 13:39:38 +0700

    Done Card Layout without sound Icon

--- data/VocabularyData.js
+++ data/VocabularyData.js
@@ -5,13 +5,13 @@ const flatListData = [
         'img': 'https://upload.wikimedia.org/wikipedia/vi/thumb/5/5f/Original_Doge_meme.jpg/300px-Original_Doge_meme.jpg'
     },
     {
-        'word': 'a',
+        'word': 'agree',
         'pronoun': '/a\'/',               
-        'img': 'https://upload.wikimedia.org/wikipedia/vi/thumb/5/5f/Original_Doge_meme.jpg/300px-Original_Doge_meme.jpg'
+        'img': 'https://i.ytimg.com/vi/FreV-974exc/maxresdefault.jpg'
     },{
-        'word': 'b',
+        'word': 'canncellation',
         'pronoun': '/a\'/',               
-        'img': 'https://upload.wikimedia.org/wikipedia/vi/thumb/5/5f/Original_Doge_meme.jpg/300px-Original_Doge_meme.jpg'
+        'img': 'https://pbs.twimg.com/profile_images/680332549529427968/kIgKrHWI_400x400.jpg'
     },{
         'word': 'c',
         'pronoun': '/a\'/',               
