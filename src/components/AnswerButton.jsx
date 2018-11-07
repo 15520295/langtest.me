e1c11804c308a0f73d393e24902242b3e7ae6458
Pham Hung Vy <vyphamhung10@gmail.com>
2018-11-07 20:16:55 +0700

    Question UI done

--- src/components/AnswerButton.jsx
+++ src/components/AnswerButton.jsx
@@ -1,32 +1,56 @@
 import React from 'react';
 import {StyleSheet} from 'react-native';
-import { Button, Container, Text, View } from 'native-base';
-
+import { Button, Container, Text, View, Icon } from 'native-base';
+import {robotoWeights} from 'react-native-typography';
 export default class AnswerButton extends React.Component{
     render(){
+        if(false){
+            const style = stylesCorrect;
+            return (            
+                <Button light style={style.answerButton} onPress={this.props.onPress}>
+                    <View style={style.answerCircle}>
+                        <Icon style={style.checkIcon} android='md-checkmark' ios='ios-checkmark'/>
+                    </View>
+                    <Text uppercase={false} allowFontScaling={true} style={style.answerText}>{this.props.text}</Text>
+                </Button>);
+        }
+
+        if(true){
+            const style = stylesIncorrect;
+            return (            
+                <Button light style={style.answerButton} onPress={this.props.onPress}>
+                    <View style={style.answerCircle}>
+                        <Icon style={style.checkIcon} type="FontAwesome" android="remove" ios="remove" />   
+                    </View>
+                    <Text uppercase={false} allowFontScaling={true} style={style.answerText}>{this.props.text}</Text>
+                </Button>);
+        }
+
+        const style = stylesNormal;
         return (
-            <Button border light style={styles.answerButton} onPress={this.props.onPress}>
-                <View style={styles.answerCircle}></View>
-                <Text uppercase={false} allowFontScaling={true} style={styles.answerText}>{this.props.text}</Text>
+            <Button border light style={style.answerButton} onPress={this.props.onPress}>
+                <View style={style.answerCircle}></View>
+                <Text uppercase={false} allowFontScaling={true} style={style.answerText}>{this.props.text}</Text>
             </Button>);
     }
 }
 
-const styles = StyleSheet.create({
+const stylesNormal = StyleSheet.create({
     answerButton: {
         flex: 1,
         borderRadius: 30,
-        borderLeftWidth: 2,
-        borderRightWidth: 2,
-        borderTopWidth: 2,
-        borderBottomWidth: 2,
-        borderColor: '#B3B3B3'
+        borderLeftWidth: 1,
+        borderRightWidth: 1,
+        borderTopWidth: 1,
+        borderBottomWidth: 1,
+        borderColor: '#B3B3B3',
+        shadowRadius: 0
     },
     answerText: {
         color: '#4F4F4F',
         fontSize: 16,
-        fontWeight: '200',
-        flex: 1
+        flex: 1,
+        ...robotoWeights.light
     },
     answerCircle: {
         height: '70%',
@@ -41,4 +65,64 @@ const styles = StyleSheet.create({
 });
 
 
+const stylesCorrect = StyleSheet.create({
+    answerButton: {
+        flex: 1,
+        borderRadius: 30,
+        borderWidth: 0,
+        backgroundColor: '#46C00D',
+        shadowRadius: 0
+    },
+    answerText: {
+        color: '#FFFFFF',
+        fontSize: 16,
+        flex: 1,
+        ...robotoWeights.regular
+    },
+    answerCircle: {
+        height: '70%',
+        borderRadius: 25,
+        aspectRatio: 1,
+        marginTop: 10,
+        marginLeft: 10,
+        marginBottom: 10,
+        marginRight: 0,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#46C00D'
+    },
+    checkIcon: {
+        color: '#FFFFFF'
+    }
+});
 
+const stylesIncorrect = StyleSheet.create({
+    answerButton: {
+        flex: 1,
+        borderRadius: 30,
+        borderWidth: 0,
+        backgroundColor: '#EF2121',
+        shadowRadius: 0
+    },
+    answerText: {
+        color: '#FFFFFF',
+        fontSize: 16,
+        flex: 1,
+        ...robotoWeights.regular
+    },
+    answerCircle: {
+        height: '70%',
+        borderRadius: 25,
+        aspectRatio: 1,
+        marginTop: 10,
+        marginLeft: 10,
+        marginBottom: 10,
+        marginRight: 0,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#EF2121'
+    },
+    checkIcon: {
+        color: '#FFFFFF'
+    }
+});
--- src/screen/QuestionScreen.jsx
+++ src/screen/QuestionScreen.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Container, Header, Left, Body, Right, Button, Icon, Title, View, Text, Content} from 'native-base';
-import {StyleSheet} from 'react-native';
+import {StyleSheet, TouchableOpacity} from 'react-native';
 import AnswerButton from '../components/AnswerButton';
+import {robotoWeights} from 'react-native-typography';
 
 export default class QuestionScreen extends React.Component {
     render() {
         return (
             <Container style={styles.container}>
-                <Header>
+                <Header androidStatusBarColor="#0076BF" style={{backgroundColor: '#019AE8'}}>
                     <Left>
                         <Button transparent>
                             <Icon android='md-arrow-back' ios='ios-arrow-back' />
@@ -28,10 +29,20 @@ export default class QuestionScreen extends React.Component {
                     </Right>
                 </Header>
                 <Content>
+                    <View style={styles.navigationView}>
+                        <Icon style={{color: '#019AE8'}} android="md-arrow-back" ios="ios-arrow-back" /> 
+                        <View style={{flexDirection: 'row'}}>
+                            <TouchableOpacity>
+                                <Text style={{fontSize: 18,color: '#019AE8'}}>1</Text>
+                            </TouchableOpacity>
+                            <Text style={{fontSize: 18}}>/3</Text>
+                        </View>
+                        <Icon style={{color: '#019AE8'}} android="md-arrow-forward" ios="ios-arrow-forward" /> 
+                    </View>
                     <View style={styles.questionView}>
                         <Text adjustsFontSizeToFit minimumFontScale={.5} style={styles.questionText}>
                     4?. In the hope of finding fresh ideas he used the internet to help him by using as many search ....... as he could
-    discover  using as many search ....... as he could discover sing as many search ....... as he could discover
+    discover  using as many search ....... as he could discover
                         </Text>
                     </View>
                     <View style={styles.answerButton}>
@@ -61,26 +72,34 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         justifyContent: 'center'
     },
+    navigationView: {
+        flex: 1,
+        justifyContent: 'space-between',
+        flexDirection: 'row',
+        marginLeft: 30,
+        marginRight: 30,
+        marginTop: 15
+    },
     questionView: {
         flex: 1,
         marginLeft: 30,
         marginRight: 30,
-        marginTop: 40,
+        marginTop: 20,
         marginBottom: 10,
         height: 120
     },
     questionText: {
         color: '#4F4F4F',
-        // fontSize: 18,
         fontWeight: '200',
         textAlign: 'justify',
-        // height: 120
+        ...robotoWeights.light
     },
     answerButton: {
         flex: 1,
         marginLeft: 30,
         marginRight: 30,
         marginBottom: 10,
-        height: 60
+        height: 60,
+        shadowRadius: 0
     }
 });
