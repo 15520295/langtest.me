60764b2831a2f205cfc75ccb850a1a81741ad7f0
15520065 <15520065@gm.uit.edu.vn>
2018-12-04 00:07:54 +0700

    Update Data

--- src/data/TopicData.js
+++ src/data/TopicData.js
@@ -1,38 +1,28 @@
 const topicFlatListData = [
     {
-        'name': 'contract',
+        'name': 'Contract',
         'count': '12',               
-        'img': 'https://upload.wikimedia.org/wikipedia/vi/thumb/5/5f/Original_Doge_meme.jpg/300px-Original_Doge_meme.jpg'
+        'img': 'https://www.iamexpat.nl/sites/default/files/styles/article--full/public/oldimages/67cddc45e6e8c166afe752d0b5e0866c1441700680.jpg?itok=1VUc7csy'
     },
     {
         'name': 'Marketing',
         'count': '12',               
-        'img': 'https://upload.wikimedia.org/wikipedia/vi/thumb/5/5f/Original_Doge_meme.jpg/300px-Original_Doge_meme.jpg'
+        'img': 'http://2.pik.vn/2017691a2d40-c263-42e9-bf3d-b8ae2635f81f.jpg'
     },
     {
         'name': 'Warranties',
         'count': '12',               
-        'img': 'https://upload.wikimedia.org/wikipedia/vi/thumb/5/5f/Original_Doge_meme.jpg/300px-Original_Doge_meme.jpg'
+        'img': 'https://www.affordablewindowstexas.com/wp-content/uploads/2017/04/warranty.png'
     },
     {
-        'name': 'contract1',
-        'count': '12',               
-        'img': 'https://upload.wikimedia.org/wikipedia/vi/thumb/5/5f/Original_Doge_meme.jpg/300px-Original_Doge_meme.jpg'
+        'name': 'Business Planning',
+        'count': '12',
+        'img': 'https://blog.postofficeshop.co.uk/wp-content/uploads/2014/07/Business-Plan-1.jpg'
     },
     {
-        'name': 'contract2',
-        'count': '12',               
-        'img': 'https://upload.wikimedia.org/wikipedia/vi/thumb/5/5f/Original_Doge_meme.jpg/300px-Original_Doge_meme.jpg'
-    },
-    {
-        'name': 'contract3',
-        'count': '12',               
-        'img': 'https://upload.wikimedia.org/wikipedia/vi/thumb/5/5f/Original_Doge_meme.jpg/300px-Original_Doge_meme.jpg'
-    },
-    {
-        'name': 'contract4',
-        'count': '12',               
-        'img': 'https://upload.wikimedia.org/wikipedia/vi/thumb/5/5f/Original_Doge_meme.jpg/300px-Original_Doge_meme.jpg'
+        'name': 'Conferences',
+        'count': '12',
+        'img': 'https://www.affordablewindowstexas.com/wp-content/uploads/2017/04/warranty.png'
     },
 ];
 export default topicFlatListData;
