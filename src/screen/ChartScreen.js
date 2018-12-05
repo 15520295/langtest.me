d187180430999d3b2b56c787f4a78d544c6f793b
huydaoduc <15520295>
2018-12-05 15:15:38 +0700

    Update expo lib, Update design ChartScreen

--- src/screen/ChartScreen.js
+++ src/screen/ChartScreen.js
@@ -1,73 +1,100 @@
 import React from 'react';
-import {ScrollView, StyleSheet, Text, requireNativeComponent, View} from 'react-native';
-// import { ExpoLinksView } from '@expo/samples';
-import PureChart from 'react-native-pure-chart';
+import {StyleSheet, View, ScrollView, Platform} from 'react-native';
+import {
+    VictoryBar,
+    VictoryChart,
+    VictoryTheme,
+    VictoryAxis,
+    VictoryPolarAxis,
+} from 'victory-native';
+
+const data = [
+    {quarter: 1, earnings: 13000},
+    {quarter: 2, earnings: 16500},
+    {quarter: 3, earnings: 14250},
+    {quarter: 4, earnings: 19000}
+];
 
 export default class ChartScreen extends React.Component {
-
-    static navigationOptions = {
-        header: null // !!! Hide Header
-        // title:'Home 1',
-        // // header: { visible:false },
-        // drawerIcon:(
-        //     <Image source={require('../../assets/images/home.png')}
-        //            style={{height: 24, width: 24}}
-        //     />
-        // )
-    };
-
     render() {
-        let sampleData = [
-            {
-                seriesName: 'series1',
-                data: [
-                    {x: '2018-02-01', y: 30},
-                    {x: '2018-02-02', y: 200},
-                    {x: '2018-02-03', y: 170},
-                    {x: '2018-02-04', y: 250},
-                    {x: '2018-02-05', y: 10}
-                ],
-                color: '#297AB1'
-            },
-            {
-                seriesName: 'series2',
-                data: [
-                    {x: '2018-02-01', y: 20},
-                    {x: '2018-02-02', y: 100},
-                    {x: '2018-02-03', y: 140},
-                    {x: '2018-02-04', y: 550},
-                    {x: '2018-02-05', y: 40}
-                ],
-                color: '#b1af31'
-            },
-            {
-                seriesName: 'series3',
-                data: [
-                    {x: '2018-03-01', y: 20},
-                    {x: '2018-03-02', y: 50},
-                    {x: '2018-03-03', y: 550},
-                    {x: '2018-03-04', y: 200},
-                    {x: '2018-03-05', y: 140}
-                ],
-                color: '#f1ae31'
-            }
-        ]
-
         return (
-            <View style={{flex:1, justifyContent:'center',alignContent:'center'}}>
-                <PureChart type={'line'}
-                       style={styles.container}
-                       data={sampleData}
-                       width={'100%'}
-                        height={200}
-                       customValueRenderer={(index, point) => {
-                           if (index % 2 === 0) return null;
-                           return (
-                               <Text style={{textAlign: 'center'}}>{point.y}</Text>
-                           );
-                       }}/>
+            <View style={styles.container}>
+                <ScrollView
+                    style={styles.container}
+                    contentContainerStyle={styles.contentContainer}
+                >
+                    {/* <VictoryChart minDomain={{ y: 0 }}>
+                <VictoryLine data={data} 
+                  animate={{
+                    duration: 2000,
+                    onLoad: { duration: 1000 }
+                  }}/>
+            </VictoryChart>
+
+            <VictoryChart minDomain={{ y: 0 }}>
+                <VictoryLine data={data1} 
+                  animate={{
+                    duration: 2000,
+                    onLoad: { duration: 1000 }
+                  }}/>
+            </VictoryChart> */}
+
+                    <VictoryChart polar
+                                  theme={VictoryTheme.material}
+                    >
+                        {
+                            ['Grammar', 'Listening', 'Speaking', 'Vocabulary', 'Reading'].map((d, i) => {
+                                return (
+                                    <VictoryPolarAxis dependentAxis
+                                                      key={i}
+                                                      label={d}
+                                                      labelPlacement="perpendicular"
+                                                      style={{tickLabels: {fill: 'none'}}}
+                                                      axisValue={i}
+                                                      animate={{
+                                                          duration: 2000,
+                                                          easing: 'bounce'
+                                                      }}
+                                    />
+                                );
+                            })
+                        }
+                        <VictoryBar
+                            style={{data: {fill: 'tomato', width: 25}}}
+                            data={[
+                                {x: 0, y: 10},
+                                {x: 1, y: 25},
+                                {x: 2, y: 40},
+                                {x: 3, y: 50},
+                                {x: 4, y: 50}
+                            ]}
+                        />
+                    </VictoryChart>
+
+                    <VictoryChart
+                        // domainPadding will add space to each side of VictoryBar to
+                        // prevent it from overlapping the axis
+                        domainPadding={20}
+                    >
+                        <VictoryAxis
+                            // tickValues specifies both the number of ticks and where
+                            // they are placed on the axis
+                            tickValues={[1, 2, 3, 4]}
+                            tickFormat={['01/11/2018', '02/11/2018', '03/11/2018', '04/11/2018']}
+                        />
+                        <VictoryAxis
+                            dependentAxis
+                            // tickFormat specifies how ticks should be displayed
+                            tickFormat={(x) => (`${x / 1000}k`)}
+                        />
+                        <VictoryBar
+                            data={data}
+                            x="quarter"
+                            y="earnings"
+                        />
+                    </VictoryChart>
+                </ScrollView>
             </View>
-            
         );
     }
 }
@@ -75,9 +102,131 @@ export default class ChartScreen extends React.Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        paddingTop: 15,
         backgroundColor: '#fff',
+    },
+    developmentModeText: {
+        marginBottom: 20,
+        color: 'rgba(0,0,0,0.4)',
+        fontSize: 14,
+        lineHeight: 19,
+        textAlign: 'center',
+    },
+    contentContainer: {
+        paddingTop: 30,
+    },
+    welcomeContainer: {
         alignItems: 'center',
-
+        marginTop: 10,
+        marginBottom: 20,
+    },
+    welcomeImage: {
+        width: 100,
+        height: 80,
+        resizeMode: 'contain',
+        marginTop: 3,
+        marginLeft: -10,
+    },
+    getStartedContainer: {
+        alignItems: 'center',
+        marginHorizontal: 50,
+    },
+    homeScreenFilename: {
+        marginVertical: 7,
+    },
+    codeHighlightText: {
+        color: 'rgba(96,100,109, 0.8)',
+    },
+    codeHighlightContainer: {
+        backgroundColor: 'rgba(0,0,0,0.05)',
+        borderRadius: 3,
+        paddingHorizontal: 4,
+    },
+    getStartedText: {
+        fontSize: 17,
+        color: 'rgba(96,100,109, 1)',
+        lineHeight: 24,
+        textAlign: 'center',
+    },
+    tabBarInfoContainer: {
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        right: 0,
+        ...Platform.select({
+            ios: {
+                shadowColor: 'black',
+                shadowOffset: {height: -3},
+                shadowOpacity: 0.1,
+                shadowRadius: 3,
+            },
+            android: {
+                elevation: 20,
+            },
+        }),
+        alignItems: 'center',
+        backgroundColor: '#fbfbfb',
+        paddingVertical: 20,
+    },
+    tabBarInfoText: {
+        fontSize: 17,
+        color: 'rgba(96,100,109, 1)',
+        textAlign: 'center',
+    },
+    navigationFilename: {
+        marginTop: 5,
+    },
+    helpContainer: {
+        marginTop: 15,
+        alignItems: 'center',
+    },
+    helpLink: {
+        paddingVertical: 15,
+    },
+    helpLinkText: {
+        fontSize: 14,
+        color: '#2e78b7',
+    },
+    parent: {
+        width: '100%',
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        marginTop: 50,
+    },
+    child: {
+        width: '48%',
+        margin: '1%',
+        aspectRatio: 1,
+        borderRadius: 20,
+        borderWidth: 1,
+        borderColor: '#fff',
+    },
+    gridView: {
+        paddingTop: 25,
+        flex: 1,
+    },
+    itemContainer: {
+        justifyContent: 'flex-end',
+        borderRadius: 5,
+        padding: 10,
+        height: 150,
+    },
+    itemName: {
+        fontSize: 16,
+        color: '#fff',
+        fontWeight: '600',
+    },
+    itemCode: {
+        fontWeight: '600',
+        fontSize: 12,
+        color: '#fff',
+    },
+    drawImage: {
+        height: 100,
+        width: 100,
+        borderRadius: 75,
+    },
+    navBar: {
+        height: 100,
+        width: 100,
     },
 });
