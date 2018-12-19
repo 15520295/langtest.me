e36ef1316b79e57761f5c8f3ce13460539ed170c
Pham Hung Vy <vyphamhung10@gmail.com>
2018-12-19 09:52:37 +0700

    Add data

--- src/App.js
+++ src/App.js
@@ -142,7 +142,8 @@ export default class App extends React.Component {
 
 const QuestionStack = createSwitchNavigator({
     Questions: QuizScreen,
-    Results: ResultScreen
+    Results: ResultScreen,
+    LeaderBoard: LeaderBoardScreen
 }, {
     initialRouteName: 'Questions'
 });
